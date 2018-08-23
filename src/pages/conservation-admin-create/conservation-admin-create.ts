3061ed4f6c3ca03291267ff572d289c585d84197
Tristan Rothman <tristan@Tristans-MBP.mshome.net>
2018-08-23 17:36:03 +0200

    rewards verification added

--- src/pages/conservation-admin-create/conservation-admin-create.ts
+++ src/pages/conservation-admin-create/conservation-admin-create.ts
@@ -30,8 +30,8 @@ export class ConservationAdminCreatePage {
       {
         console.log(data);
         var jsonResp = JSON.parse(data.text());
-        //alert(data.text());
         this.areas = jsonResp.areas;
+        console.log(this.areas);
       }
     );
   }
@@ -50,14 +50,14 @@ export class ConservationAdminCreatePage {
       "email":"",
       "name":"",
       "surname":"",
-      "area":""
+      "area":0
     };
 
     jsonArr.username = value.username;
     jsonArr.email = value.email;
     jsonArr.name = value.fname;
     jsonArr.surname = value.sname;
-    jsonArr.area = value.carea;
+    jsonArr.area = parseInt(value.carea);
 
     this.http.post("/admin/add", jsonArr).subscribe
     (
